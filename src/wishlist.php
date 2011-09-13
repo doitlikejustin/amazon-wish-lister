fcc52b2f509413ec349d7abab51df0e9c109bfe8
Justin Scarpetti <justin@justinscarpetti.com>
2011-09-13 16:31:57 -0700

    fixed json parameter and added xml output

--- src/wishlist.php
+++ src/wishlist.php
@@ -12,15 +12,20 @@
 
 require_once('phpquery.php');
 
+//?id=YOUR_AMAZON_ID
 //get the amazon id or force an ID if none is passed
 if(isset($_GET['id'])) $amazon_id = $_GET['id'];
 else $amazon_id = '37XI10RRD17X2';
 
+//?reveal=unpurchased
+//checks what to reveal (unpurchased, all, or purchased)... defaults to unpurchased
 if($_GET['reveal'] == 'unpurchased') $reveal = 'reveal=unpurchased';
 else if($_GET['reveal'] == 'all') $reveal = 'reveal=all';
 else if($_GET['reveal'] == 'purchased') $reveal = 'reveal=purchased';
 else $reveal = 'reveal=unpurchased';
 
+//?sort=date
+//sorting options (date, title, price-high, price-low, updated, priority)
 if($_GET['sort'] == 'date') $sort = 'sort=date-added';
 else if($_GET['sort'] == 'title') $sort = 'sort=universal-title';
 else if($_GET['sort'] == 'price-low') $sort = 'sort=universal-price';
@@ -55,13 +60,30 @@ foreach($items as $item)
 	}
 }
 
+//format the xml
+function xml_ecode($array) {
+
+	$xml = '';
+
+	if (is_array($array) || is_object($array)) {
+		foreach ($array as $key=>$value) {
+			if (is_numeric($key)) {
+				$key = 'item'; 
+			}
+			
+			//create the xml tags
+			$xml .= '<' . $key . '>' . xml_ecode($value) . '</' . $key . '>';
+		}
+	} else { $xml = htmlspecialchars($array, ENT_QUOTES); }
+
+	return $xml;
+}
+
 //?format=json
-if($_REQUEST['format'] == 'json') { json_encode($array); }
-
-//?format=array
-if($_REQUEST['format'] == 'array') { print_r($array); }
-
-//?format=xml
-else { print_r($array); }
+//format the wishlist (json, xml, or php array object) defaults to json
+if($_REQUEST['format'] == 'json') { echo json_encode($array); }
+else if($_REQUEST['format'] == 'xml') { echo xml_ecode($array); }
+else if($_REQUEST['format'] == 'array') { print_r($array); }
+else { echo json_encode($array); }
 
 ?>
